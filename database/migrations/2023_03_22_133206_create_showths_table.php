efe416f41c341744654ce1fbc543e17e84f35117
SasaAndjelkovic <sandjelkovickg@gmail.com>
2023-03-22 15:16:51 +0100

    Add migrations

--- database/migrations/2023_03_22_133206_create_showths_table.php
+++ database/migrations/2023_03_22_133206_create_showths_table.php
@@ -11,11 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('showths', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('description');
+            $table->string('author');
             $table->timestamps();
         });
     }
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('showths');
     }
 };
