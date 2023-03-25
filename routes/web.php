15abbfa4e8b3c294938cc16db76dd2c82a12ab6f
SasaAndjelkovic <sandjelkovickg@gmail.com>
2023-03-25 09:25:03 +0100

    Add avatar routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ShowthController;
+use App\Http\Controllers\AvatarController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/showths', [ShowthController::class, 'index'])->name('showths.index');
@@ -9,3 +10,12 @@ Route::get('/showths/{showth}/edit', [ShowthController::class, 'edit'])->name('s
 Route::post('/showths/store', [ShowthController::class, 'store'])->name('showths.store');
 Route::put('/showths/{showth}/update', [ShowthController::class, 'update'])->name('showths.update');
 Route::delete('/showths/{showth}/delete', [ShowthController::class, 'destroy'])->name('showths.destroy');
+
+Route::get('/avatars', [AvatarController::class, 'index'])->name('avatars.index');
+Route::get('/avatars/create', [AvatarController::class, 'create'])->name('avatars.create');
+Route::get('/avatars/{avatar}/edit', [AvatarController::class, 'edit'])->name('avatars.edit');
+Route::post('/avatars/store', [AvatarController::class, 'store'])->name('avatars.store');
+Route::put('/avatars/{avatar}/update', [AvatarController::class, 'update'])->name('avatars.update');
+Route::delete('/avatars/{avatar}/delete', [AvatarController::class, 'destroy'])->name('avatars.destroy');
+
+
